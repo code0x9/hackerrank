8111c71f8578af436cd9a6d643d7fb4dffef544a
Mark Lee <code0x9@gmail.com>
2018-11-02 00:11:16 +0900

    done arrayManipulation

--- main_test.go
+++ main_test.go
@@ -9,47 +9,33 @@ import (
 func Test(t *testing.T) {
 	assert.Equal(t, int64(200),
 		arrayManipulation(5, [][]int32{
-			[]int32{1, 2, 100},
-			[]int32{2, 5, 100},
-			[]int32{3, 4, 100},
+			{1, 2, 100},
+			{2, 5, 100},
+			{3, 4, 100},
 		}))
 	assert.Equal(t, int64(10),
 		arrayManipulation(10, [][]int32{
-			[]int32{1, 5, 3},
-			[]int32{4, 8, 7},
-			[]int32{6, 9, 1},
+			{1, 5, 3},
+			{4, 8, 7},
+			{6, 9, 1},
 		}))
 }
 
+// use "stateful" x value
 func arrayManipulation(n int32, queries [][]int32) int64 {
 	r := make([]int, n)
-	max := -1
-	for i := 0; i < int(n); i++ {
-		var sum int
-		for _, query := range queries {
-			a, b, k := int(query[0])-1, int(query[1])-1, query[2]
-			if i >= a && i <= b {
-				sum += int(k)
-			}
-		}
-		r[i] = sum
-		if max < sum {
-			max = sum
+	for _, query := range queries {
+		a, b, k := int(query[0])-1, int(query[1])-1, int(query[2])
+		r[a] += k
+		if b+1 < int(n) {
+			r[b+1] -= k
 		}
 	}
-	return int64(max)
-}
-
-func arrayManipulationSimple(n int32, queries [][]int32) int64 {
-	r := make([]int, n)
-	max := int64(-1)
-	for _, query := range queries {
-		a, b, k := query[0], query[1], query[2]
-		for i := a - 1; i < b; i++ {
-			r[i] += int(k)
-			if max < int64(r[i]) {
-				max = int64(r[i])
-			}
+	max, x := -1, 0
+	for _, v := range r {
+		x += v
+		if max < x {
+			max = x
 		}
 	}
 	return int64(max)
